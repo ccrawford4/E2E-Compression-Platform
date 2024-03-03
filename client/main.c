c8530968f994ac2f8545c619bf401fd591051f4f
Calum <calumcrawford9@gmail.com>
2024-03-03 13:11:18 -0800

    Resolved compile issues

--- client/main.c
+++ client/main.c
@@ -3,15 +3,33 @@
 #include <stdlib.h>
 // parse_json()
 
+#define PATH_PREFIX "./"
+
 int main(int argc, char**argv) {
     if (argc != 2) {
         printf("usage: \n");
         printf("./compdetect_client <file_name.json>\n");
-        exit(EXIT_FAILURE);
+        return EXIT_FAILURE;
     }
-   // char* server_address = parse_json("server_ip");
-   // char* server_port = parse_json("UDP_dest_port_number");
+    char* file_name = argv[1];
+    size_t size = snprintf(NULL, 0, "%s%s", PATH_PREFIX, file_name) + 1;
 
-   // establish_connect(server_address, server_port);
+    char* full_path = malloc(size);
+    if (full_path = NULL) {
+        perror("Failed to allocate memory");
+        return EXIT_FAILURE;
+    }
+
+    snprintf(full_path, size, "%s%s", PATH_PREFIX, file_name);
+
+    const char* server_address = get_value(full_path, "server_ip");
+    const char* server_port = get_value(full_path, "UDP_dest_port_number");
+
+    free(full_path);
+
+    establish_connection(server_address, server_port);
+
+    free(server_address);
+    free(server_port);
 
 }
--- client/main.h
+++ client/main.h
@@ -1,1 +1,2 @@
-void establish_connection(char* server_address, char* server_port);
+void establish_connection(const char* server_address, const char* server_port);
+const char* get_value(char* file_path, char* key);
