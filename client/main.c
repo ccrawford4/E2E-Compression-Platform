c9bb219368a2c8e52f860607462f2eddb38cad29
Calum <calumcrawford9@gmail.com>
2024-03-03 11:48:00 -0800

    imported necessary h files

--- client/main.c
+++ client/main.c
@@ -1,7 +1,13 @@
-
+#include <stdio.h>
+#include <stdlib.h>
 // parse_json()
 
 int main(int argc, char**argv) {
+    if (argc != 2) {
+        printf("usage: \n");
+        printf("./compdetect_client <file_name.json>\n");
+        exit(EXIT_FAILURE);
+    }
    // char* server_address = parse_json("server_ip");
    // char* server_port = parse_json("UDP_dest_port_number");
 
