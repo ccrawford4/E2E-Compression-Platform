0ae68fc82580a65835b6f096f1f2e192aadfb50a
Calum <calumcrawford9@gmail.com>
2024-03-04 19:53:48 -0800

    changes

--- server/main.h
+++ server/main.h
@@ -1,2 +1,9 @@
 #include "../shared/shared.h"
+
+void handle_error(int sockfd, char* error_msg);
 int init_socket(unsigned short server_port);
+int send_packets(int sockfd, char* buffer, int buffer_len);
+int recieve_packets(int sockfd, char *buffer, int buffer_len);
+void server_listen(int sockfd);
+void clean_exit();
+void close_sockets();
