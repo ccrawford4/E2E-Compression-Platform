b146d8f22b86ca2e3b120a7e146ce008ae4a2963
Calum <calumcrawford9@gmail.com>
2024-03-12 17:57:29 -0700

    Part 1 Complete!

--- server/main.c
+++ server/main.c
@@ -1,20 +1,41 @@
 #include "main.h"
 
 #define MAX_BUFFER_LEN 1000
+#define FILE_NAME "config.json"
+
+void write_contents_to_file(int sockfd, char* buffer, size_t buffer_size) {
+    FILE* fp = fopen(FILE_NAME, "wb");
+    if (fp == NULL) {
+        perror("Failed to open file");
+        exit(EXIT_FAILURE);
+    }
+    
+    size_t bytes_written = fwrite(buffer, 1, buffer_size, fp);
+    if (bytes_written < strlen(buffer)) {
+          perror("Failed to write full buffer to file");
+    }
+
+    free(buffer);
+    fclose(fp);
+}
 
 void print_out_contents(int sockfd) {
     char* buffer = (char*)malloc(MAX_BUFFER_LEN);
 
     int bytes_received = receive_packets(sockfd, buffer, MAX_BUFFER_LEN);
-    printf("%s\n", buffer);    
-    free(buffer);
+    size_t buffer_size = strlen(buffer);
+    
+    printf("%s\n", buffer);
+    write_contents_to_file(sockfd, buffer, buffer_size);
 
     if (bytes_received < 0) {
         handle_error(sockfd, "bytes_recieved");
     }
+
     char* server_msg = "File Received!";
     send_packets(sockfd, server_msg, strlen(server_msg) + 1);
 }
+
 
 int main(int argc, char**argv) {
     if (argc != 2) {
@@ -24,13 +45,10 @@ int main(int argc, char**argv) {
     }
 
     const char* server_port = argv[1];
-
     int sockfd = init_socket(server_port);
-
     int client_socket = server_listen(sockfd);
 
-    print_out_contents(client_socket);
-
+    print_out_contents(client_socket); // For testing and sending confirmation    
     close_sockets(sockfd, client_socket);
 
     return 0;
