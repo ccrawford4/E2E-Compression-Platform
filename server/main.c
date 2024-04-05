25724b6c715006256ef65d2d354ee7047d4a8403
Calum <calumcrawford9@gmail.com>
2024-04-04 18:03:40 -0700

    Started set up for POST phase with results

--- server/main.c
+++ server/main.c
@@ -1,12 +1,15 @@
 #include "main.h"
+#include <math.h>
 
 #define MAX_BUFFER_LEN 1000
 #define MAX_LINE 1000
-#define FILE_NAME "config.json"
+#define R 0.100
+#define CONFIG_FILE "config.json"
+#define RESULT_FILE "result.txt"
 
 // Write the JSON contents to the config.json file
-void write_contents_to_file(int sockfd, char* buffer, size_t buffer_size) {
-    FILE* fp = fopen(FILE_NAME, "wb");
+void write_contents_to_file(char* file_name, char* buffer, size_t buffer_size) {
+    FILE* fp = fopen(file_name, "wb");
     if (fp == NULL) {
         perror("Failed to open file");
         exit(EXIT_FAILURE);
@@ -21,22 +24,69 @@ void write_contents_to_file(int sockfd, char* buffer, size_t buffer_size) {
     fclose(fp);
 }
 
+bool calc_results(double time_one, double time_two) {
+    double diff = abs(time_one - time_two);
+    if (diff > R) {
+        return true;
+    }
+    return false;
+}
+
+void save_results(double time_one, double time_two) {
+   double diff = abs(time_one - time_two);
+   char* buffer = (char*)malloc(MAX_BUFFER_LEN);
+   size_t n = sizeof(buffer);
+   if (buffer == NULL) {
+        perror("Memory allocation failure");
+        exit(EXIT_FAILURE);
+   }
+   
+   bool found_compression = calc_results(time_one, time_two);
+   int w;
+   if (found_compression) {
+        w = snprintf(buffer, n, "Compression Detected!\n Time One: %f\tTime Two: %f\n", 
+        time_one, time_two);
+   } else {
+        w = snprintf(buffer, n, "No Compression Detected!\n Time one: %f\tTime Two: %f\n",
+        time_one, time_two);
+   }
+
+   if (w >= n) {
+       perror("Buffer length exceeded!\n");
+       exit(EXIT_FAILURE);
+   }
+
+   write_contents_to_file(RESULT_FILE, buffer, n);
+}
+
 // Prints out JSON contents from Pre-Probing TCP Connection Phase
-void print_out_contents(int sockfd) {
+void recv_config_file(int sockfd) {
     char* buffer = (char*)malloc(MAX_BUFFER_LEN);
 
     int bytes_received = receive_packets(sockfd, buffer, MAX_BUFFER_LEN);
     size_t buffer_size = strlen(buffer);
     
-    printf("%s\n", buffer);
-    write_contents_to_file(sockfd, buffer, buffer_size);
+    write_contents_to_file(CONFIG_FILE, buffer, buffer_size);
 
     if (bytes_received < 0) {
         handle_error(sockfd, "bytes_recieved");
     }
 
-    char* server_msg = "File Received!";
+    char* server_msg = "Config File Received!";
     send_packets(sockfd, server_msg, strlen(server_msg) + 1);
+}
+
+void send_results(int sockfd) {
+    char* buffer = "hello"; 
+    int n = strlen(buffer);
+    // read the file contents into the buffer
+    
+    int packets = send_packets(sockfd, buffer, n);
+    if (packets != n) {
+        perror("ERROR! Not all the packets were received");
+        close(sockfd);
+        exit(EXIT_FAILURE);
+    }
 }
 
 // Receives UDP packets given a port number
@@ -63,12 +113,16 @@ void recv_udp_packets(int sockfd, unsigned int port_number, int expected_bytes) 
 }
 
 // Establishes a TCP Connection
-void establish_tcp_connection(unsigned int server_port) {
+void establish_tcp_connection(unsigned int server_port, bool pre_prob) {
     int tcp_socket = init_socket(server_port, SOCK_STREAM);
     int client_socket = server_listen(tcp_socket);
     
-    // Establish TCP Connection
-    print_out_contents(client_socket); // For testing and sending confirmation   
+    if (pre_prob) {
+        recv_config_file(client_socket);
+    } else {
+       send_results(client_socket); 
+    }
+     
 }
 
 
@@ -114,7 +168,7 @@ double calc_stream_time(unsigned int server_wait_time, struct sockaddr_in cliadd
 
 // Probing Phase -> receive UDP packets from the sender
 void probing_phase() { 
-    unsigned int udp_port = (unsigned int)atoi(get_value(FILE_NAME, "UDP_dest_port_number"));
+    unsigned int udp_port = (unsigned int)atoi(get_value(CONFIG_FILE, "UDP_dest_port_number"));
     if (udp_port == 0) {
         handle_error(udp_port, "Invalid UDP_dest_port_number");
         exit(EXIT_FAILURE);
@@ -144,13 +198,13 @@ void probing_phase() {
     }
 
     int n;
-    unsigned int server_wait_time = (unsigned int)atoi(get_value(FILE_NAME, "server_wait_time"));
-    unsigned int client_wait_time = (unsigned int)atoi(get_value(FILE_NAME, "measurement_time"));
+    unsigned int server_wait_time = (unsigned int)atoi(get_value(CONFIG_FILE, "server_wait_time"));
+    unsigned int client_wait_time = (unsigned int)atoi(get_value(CONFIG_FILE, "measurement_time"));
     double time_one = calc_stream_time(server_wait_time, cliaddr, sockfd);
-    printf("Time one: %.10f", time_one);
     wait(client_wait_time);
     double time_two = calc_stream_time(server_wait_time, cliaddr, sockfd);
-    printf("Time two: %.10f", time_two);
+
+    save_results(time_one, time_two);
 
 }
 
@@ -166,16 +220,25 @@ int main(int argc, char**argv) {
         printf("ERROR! %s Is Not A Valid Port Number\n", argv[1]);
         return EXIT_FAILURE;
     }
+
     
     time_t rawtime;
     struct tm * timeinfo;
 
-    establish_tcp_connection(server_port);         // Pre-Probing TCP Phase Connection
+    establish_tcp_connection(server_port, true);         // Pre-Probing TCP Phase Connection
     
     time (&rawtime);
     timeinfo = localtime (&rawtime);
     printf("Current time before probing phase: %s\n", asctime (timeinfo));
     probing_phase();                               // Probing Phase
+
+    unsigned int post_prob_tcp_port = (unsigned int) atoi(get_value(CONFIG_FILE, "TCP_POSTPROB_port_number"));
+    if (post_prob_tcp_port == 0) {
+        printf("ERROR! %d Is Not A Valid Port Number\n", post_prob_tcp_port);
+        return EXIT_FAILURE;
+    }
+
+    establish_tcp_connection(post_prob_tcp_port, false);
         
     return EXIT_SUCCESS;
 }
