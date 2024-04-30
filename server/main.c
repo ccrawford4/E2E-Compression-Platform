5269c510ed8c0c8c6f761f3f8d37ca12ae55dd73
Calum <calumcrawford9@gmail.com>
2024-04-29 20:24:47 -0700

    More comments

--- server/main.c
+++ server/main.c
@@ -3,28 +3,26 @@
 #define MAX_BUFFER_LEN 1000
 #define R 0.100
 #define CONFIG_FILE "config.json"
-#define RESULT_FILE "result.txt"
 
-#ifndef DEBUG
-#define DEBUG 0
-#endif
-
+// Boolean flag set to false
+// Will be changed if compression is detected over the network
 int compression_flag = 0;
 
+// Set the compression flag
 void set_compression_flag(int value) {
     compression_flag = value;
 }
 
-// Calculate if Compression was detected
+// Calculate if compression was detected and return true if it has been
 bool calc_results(double time_one, double time_two) {
   double diff = abs(time_one - time_two);
-  if (diff > R) {
+  if (diff > R) {                        
     return true;
   }
   return false;
 }
 
-// Receives and saves the JSON contents from Pre-Probing TCP Connection Phase
+// Receives and saves the config file from Pre-Probing TCP Connection Phase
 void recv_config_file(int sockfd) {
   char *buffer = (char *)malloc(MAX_BUFFER_LEN);
   if (buffer == NULL) {
@@ -32,9 +30,11 @@ void recv_config_file(int sockfd) {
     exit(EXIT_FAILURE);
   }
 
+  // Receives the config file from the client
   int bytes_received = receive_bytes(sockfd, buffer, MAX_BUFFER_LEN, 0);
   size_t buffer_size = strlen(buffer);
 
+  // Writes the bytes received into a new config file in order to store the data for later use
   write_contents_to_file(CONFIG_FILE, buffer, buffer_size);
 
   if (bytes_received < 0) {
@@ -51,12 +51,14 @@ void send_results(int sockfd) {
         exit(EXIT_FAILURE);
   }
 
+  // Populate the buffer based on compression results
   if (compression_flag) {
        strcpy(buffer, "Compression Detected!\n");
   } else {
        strcpy(buffer, "No Compression Detected!\n");
   }
 
+  // Send to the results to the client
   ssize_t packets = send(sockfd, buffer, strlen(buffer), 0);
 
   if (packets < 0) {
@@ -68,21 +70,28 @@ void send_results(int sockfd) {
 
 // Establishes a TCP Connection and based on the phase performs operations
 void establish_tcp_connection(unsigned int server_port, bool pre_prob) {
-  int tcp_socket = init_socket(server_port, SOCK_STREAM);
-  int client_socket = server_listen(tcp_socket);
+  // TCP socket used for PRE-PROB TCP phase
+  int tcp_socket = init_socket(server_port, SOCK_STREAM);               
 
+  // TCP socket used for the POST-PROB TCP phase
+  int client_socket = server_listen(tcp_socket);                         
+
+  // During PRE-PROB phase receive the config file - otherwise send the compression results
   if (pre_prob) {
     recv_config_file(client_socket);
   } else {
     send_results(client_socket);
   }
   
+  // Close both sockets after use
   close(tcp_socket);
   close(client_socket);
 }
 
+// Receives the UDP packets and calculates the stream time
 double calc_stream_time(unsigned int server_wait_time,
                         struct sockaddr_in cliaddr, int sockfd) {
+  // Buffer used to receive the payload
   char *buffer = (char *)malloc(MAX_BUFFER_LEN); // Allocate buffer correctly
   if (buffer == NULL) {
     handle_error(sockfd, "Memory allocation failure");
@@ -101,57 +110,67 @@ double calc_stream_time(unsigned int server_wait_time,
     perror("setsockopt failed");
   }
 
+  // Initalize timespec structs
+  // start_time = start of UDP stream, end_time = end of UDP stream
+  // current_time = current time used for UDP stream timeout
   struct timespec start_time, current_time, end_time;
   clock_gettime(CLOCK_MONOTONIC, &start_time);
-  end_time = start_time; // Initialize end_time to start_time
+  end_time = start_time; 
 
-#if DEBUG
-  print_time(start_time);
-#endif
 
-  // Calculates the time it takes for the packet train to be received
   while (true) {
+    // Get the current time and calculate how many seconds have elapsed
     clock_gettime(CLOCK_MONOTONIC, &current_time);
     double elapsed = (current_time.tv_sec - start_time.tv_sec) +
                      (current_time.tv_nsec - start_time.tv_nsec) / 1000000000.0;
 
+    // If timeout has been reached then end the loop
     if (elapsed >= server_wait_time) {
       break;
     }
 
+    // Receive the UDP packet from the client
     n = recvfrom(sockfd, buffer, MAX_BUFFER_LEN - 1, 0,
                  (struct sockaddr *)&cliaddr, &len);
     if (n > 0) {
       end_time = current_time; // Update end_time each time data is received
     }
   }
+  free(buffer);
 
+  // After the loop, calculate how long the stream took to arrive to the server
   double total_elapsed = (end_time.tv_sec - start_time.tv_sec) +
                          (end_time.tv_nsec - start_time.tv_nsec) / 1000000000.0;
-
-  free(buffer); // Free allocated memory
-
-  return total_elapsed;
+    
+  return total_elapsed;     // Return the total stream time
 }
 
 // Probing Phase -> receive UDP packets from the sender
 void probing_phase(unsigned int port, unsigned int server_wait_time,
                    unsigned int client_wait_time) {
+  // Client IP address struct
   struct sockaddr_in cliaddr;
-  int sockfd = init_socket(port, SOCK_DGRAM);
+  int sockfd = init_socket(port, SOCK_DGRAM);       // Creates a UDP socket
 
   int n;
 
+  // Receive first UDP stream
   double time_one = calc_stream_time(server_wait_time, cliaddr, sockfd);
+
+  // Wait to avoid packet overlap
   wait(client_wait_time);
+
+  // Receive second UDP stream
   double time_two = calc_stream_time(server_wait_time, cliaddr, sockfd);
 
+  // Calculate results and set the compression flag accordingly
   int compression_flag = calc_results(time_one, time_two) ? 1 : 0;
   set_compression_flag(compression_flag);
 
 }
 
 int main(int argc, char **argv) {
+  // Require the user to include the receiving TCP port number for the inital TCP handshake (Pre-prob phase)
   if (argc != 2) {
     printf("usage: \n");
     printf("./compdetect_server <TCP port number>\n");
@@ -163,10 +182,12 @@ int main(int argc, char **argv) {
     printf("ERROR! %s Is Not A Valid Port Number\n", argv[1]);
     return EXIT_FAILURE;
   }
-  establish_tcp_connection(server_port,
-                           true); // Pre-Probing TCP Phase Connection
 
-  // Get the values from the JSON
+  // Establish the connection by listening for the client's request to connect
+  establish_tcp_connection(server_port,
+                           true);
+
+  // Get the necssary values from the JSON that was created in the pre-prob phase
   unsigned int udp_port =
       (unsigned int)atoi(get_value(CONFIG_FILE, "UDP_dest_port_number"));
   unsigned int server_wait_time =
@@ -182,10 +203,11 @@ int main(int argc, char **argv) {
   handle_key_error(client_wait_time, "measurement_time", CONFIG_FILE);
   handle_key_error(post_prob_tcp_port, "TCP_POSTPROB_port_number", CONFIG_FILE);
 
-  probing_phase(udp_port, server_wait_time, client_wait_time); // Probing Phase
+  // Send the UDP streams and calculate the results
+  probing_phase(udp_port, server_wait_time, client_wait_time); 
 
+  // Establish Post-Prob TCP handshake and send the compression results to the client
   establish_tcp_connection(post_prob_tcp_port,
-                           false); // Post-Prob TCP Phase connection
-
+                           false); 
   return EXIT_SUCCESS;
 }
--- server/socket.c
+++ server/socket.c
@@ -1,19 +1,21 @@
 #include "main.h"
 
-// Binds socket
-void bind_socket(int sockfd, unsigned short server_port) {
+#define MAX_BUFFER_LEN 1000
+
+// Binds a socket given the file descriptor and the port number
+void bind_socket(int sockfd, unsigned short port) {
   int optval = 1;
   if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &optval, sizeof(optval)) <
       0) {
     handle_error(sockfd, "setsockopt()");
   }
-
+  
+  // Source IP address configuration
   struct sockaddr_in sin;
   memset(&sin, 0, sizeof(sin));
-
   sin.sin_family = AF_INET;
   sin.sin_addr.s_addr = INADDR_ANY;
-  sin.sin_port = htons(server_port);
+  sin.sin_port = htons(port);
 
   if (bind(sockfd, (struct sockaddr *)&sin, sizeof(sin)) < 0) {
     handle_error(sockfd, "bind() cannot bind socket to address");
@@ -26,6 +28,7 @@ int server_listen(int sockfd) {
     handle_error(sockfd, "listen() error listening");
   }
 
+  // Accept incoming TCP connections
   struct sockaddr_in addr;
   int client_sockfd, addr_len = sizeof(addr);
   client_sockfd = accept(sockfd, (struct sockaddr *)&addr, &addr_len);
@@ -36,9 +39,9 @@ int server_listen(int sockfd) {
   return client_sockfd;
 }
 
-// Creates and configures a socket then returns the socket file descriptor
-int init_socket(unsigned int port_number, int type) {
-  int sockfd = socket(AF_INET, type, 0);
+// Creates and binds a socket given a port number and protocol
+int init_socket(unsigned int port_number, int proto) {
+  int sockfd = socket(AF_INET, proto, 0);
   if (sockfd == -1) {
     handle_error(sockfd, "socket()");
   }
@@ -47,9 +50,10 @@ int init_socket(unsigned int port_number, int type) {
 }
 
 // Receives UDP packets
-ssize_t receive_udp_payload(int sockfd, struct sockaddr *src_addr,
-                            socklen_t addrlen) {
-  char *buffer = (char *)malloc(1000);
+ssize_t receive_udp_payload(int sockfd, struct sockaddr *src_addr, 
+                            socklen_t addrlen) {  
+  // Create recv buffer
+  char *buffer = (char *)malloc(MAX_BUFFER_LEN);
   if (buffer == NULL) {
     handle_error(sockfd, "Memory Allocation");
     return EXIT_FAILURE;
@@ -57,6 +61,8 @@ ssize_t receive_udp_payload(int sockfd, struct sockaddr *src_addr,
   memset(buffer, 0, sizeof(buffer) / sizeof(char));
   size_t len = strlen(buffer);
   ssize_t bytes;
+
+  // Receive UDP packets from the client
   if ((bytes = recvfrom(sockfd, buffer, len, 0, src_addr, &addrlen)) < 0) {
     handle_error(sockfd, "recvfrom()");
     return EXIT_FAILURE;
